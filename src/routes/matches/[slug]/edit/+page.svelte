f05b90d8320ca240e29e4e6cb00ed46f4b10c95c
Renan Araujo <renan.alvs@gmail.com>
2022-12-09 23:22:55 -0300

    Fix match editing

--- src/routes/matches/[slug]/edit/+page.svelte
+++ src/routes/matches/[slug]/edit/+page.svelte
@@ -14,11 +14,13 @@
     });
 
     const checkboxFields = {
-        isPlayoff, 
-        overtime: (overtime === "true"), 
-        penaltyKicks,
-        finished
+        isPlayoff: Boolean(isPlayoff), 
+        overtime: Boolean(overtime), 
+        penaltyKicks: Boolean(penaltyKicks),
+        finished: Boolean(finished)
     }
+
+    $: console.log(checkboxFields);
 
     const checkboxLabels = (field) => {
         switch(field) {
@@ -73,11 +75,11 @@
     <div class="px-4 w-full uppercase text-sm flex flex-col justify-center items-center">
         {#if checkboxFields.overtime}
             <div class="pb-1" id="ruleLabel">{checkboxLabels("overtimeWinner")}</div>
-            <input class="text-slate-900 w-10 h-10" type="text" name="overtimeWinner" id="overtimeWinner" value="{overtimeWinner.id}" />
+            <input class="text-slate-900 w-10 h-10" type="text" name="overtimeWinner" id="overtimeWinner" value="{overtimeWinner?.id ?? ""}" />
         {/if}
         {#if checkboxFields.penaltyKicks}
             <div class="pb-1" id="ruleLabel">{checkboxLabels("penaltyKickWinner")}</div>
-            <input class="text-slate-900 w-10 h-10 text-2xl text-center" type="text" name="penaltyKickWinner" id="penaltyKickWinner" value="{penaltyKickWinner.id}" />
+            <input class="text-slate-900 w-10 h-10 text-2xl text-center" type="text" name="penaltyKickWinner" id="penaltyKickWinner" value="{penaltyKickWinner?.id ?? ""}" />
         {/if}
     </div>
     <div class="w-full text-2xl flex justify-center items-center align-middle uppercase mt-6">
